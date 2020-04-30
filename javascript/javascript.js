9bbe81e10d6f048a0761b9217984257e265b84f5
MathisBremont <49200721+MathisBremont@users.noreply.github.com>
2020-04-30 17:53:13 +0200

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,26 @@
+function changerImage(n) {
+ 
+switch (n) {
+ 
+case 1:
+document.getElementById("image1").src="../images/projets/wroomSelected.png";
+break;
+ 
+case 2:
+document.getElementById("image2").src="../images/projets/quizzactSelected.jpg";
+break;
+}
+}
+ 
+function remettreImage(n) {
+switch (n) {
+ 
+case 1:
+document.getElementById("image1").src="../images/projets/wroom.PNG";
+break;
+
+case 2:
+document.getElementById("image2").src=" ../images/projets/quizzact.jpg";
+break;
+}
+}
